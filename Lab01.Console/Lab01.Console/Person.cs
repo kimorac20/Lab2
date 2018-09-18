667979c16e036b5d7e76fd042657bae8f3276d55
Kimberly <kimorac20@gmail.com>
2018-09-17 20:03:40 -0600

    Initial commit

---
+++ Lab01.Console/Lab01.Console/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01.Logic
+{
+    public class Person
+    {
+        public Person()
+        {
+
+        }
+
+        public Person(int id, string name, string lastName)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+}
